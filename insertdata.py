ea4569286d788c064e07a03dde80be1037835a21
mengmanzbh <1361006944@qq.com>
2019-01-23 15:07:03 +0800

    OK

--- insertdata.py
+++ insertdata.py
@@ -8,15 +8,13 @@ if __name__ == '__main__':
                              db='tgapp_excel',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
-
     records = p.iget_records(file_name="plane.xlsx")
     for record in records:
-    	print(record['A'],record['B'],record['C'],record['D'],record['E'])
-    	try:
-    		with connection.cursor() as cursor:
-    		# Create a new record
-    		sql = "INSERT INTO `airport_city_triple_code` (`airport_code`, `airport_name`, `city_code`, `city_name`, `country`) VALUES (%s, %s, %s, %s, %s)"
-    		cursor.execute(sql, (record['A'], record['B'], record['C'], record['D'], record['E']))
-    	connection.commit()
+        # print(record['A'],record['B'],record['C'],record['D'],record['E'])
+        try:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO `airport_city_triple_code` (`airport_code`, `airport_name`, `city_code`, `city_name`, `country`) VALUES (%s, %s, %s, %s, %s)"
+                cursor.execute(sql, (record['A'], record['B'], record['C'], record['D'], record['E']))
+            connection.commit()
         finally:
-        	connection.close()
+            connection.close()
