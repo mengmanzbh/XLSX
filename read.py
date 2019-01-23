9d848219aed6be85cf1aa32224faf24632340517
mengmanzbh <1361006944@qq.com>
2019-01-23 14:16:16 +0800

    OK

--- read.py
+++ read.py
@@ -1,5 +1,5 @@
 import pyexcel as p
 if __name__ == '__main__':
-    records = p.iget_records(file_name="plane.csv")
+    records = p.iget_records(file_name="plane.xls")
     for record in records:
-    	print(record['airplane'],record['airname'],record['citycode'],record['cityname'])
+    	print(record['1'],record['2'],record['3'],record['4'],record['5'])
