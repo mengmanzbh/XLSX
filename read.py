48574e89f5e8049b3f734f9bb73b6b332419eccb
mengmanzbh <1361006944@qq.com>
2019-01-23 14:22:13 +0800

    OK

--- read.py
+++ read.py
@@ -2,4 +2,4 @@ import pyexcel as p
 if __name__ == '__main__':
     records = p.iget_records(file_name="plane.xls")
     for record in records:
-    	print(record['1'],record['2'],record['3'],record['4'],record['5'])
+    	print(record['A'],record['B'],record['C'],record['D'],record['E'])
