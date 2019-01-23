12e674378ff16c7f62cc13e600a2687808333f33
mengmanzbh <1361006944@qq.com>
2019-01-23 14:09:08 +0800

    O

--- read.py
+++ read.py
@@ -3,4 +3,3 @@ if __name__ == '__main__':
     records = p.iget_records(file_name="plane.csv")
     for record in records:
     	print(record)
-        print(record['airplane'], record['airname'], record['citycode'], record['cityname'])
