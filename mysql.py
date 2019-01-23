b9dff49787e11f0d53fcab67fb5368ded9ea0ba8
mengmanzbh <1361006944@qq.com>
2019-01-23 14:55:51 +0800

    OK

---
+++ mysql.py
@@ -0,0 +1,21 @@
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='123.58.7.80:3306',
+                             user='sk_admin',
+                             password='skmm94bgsn',
+                             db='tgapp_excel',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `airport_city_triple_code` (`airport_code`, `airport_name`, `city_code`, `city_name`, `country`) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(sql, ('1', '2', '3', '4', '5'))
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    connection.commit()
+finally:
+    connection.close()
